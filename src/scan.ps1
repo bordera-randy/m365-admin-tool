f7156524942f81f4de7dca278722f5296db1174b
Randy Bordeaux <randy.bordeaux@starcompliance.com>
2026-02-27 22:10:58 -0600

    Remove unused results and scan scripts for cleaner project structure

--- src/scan.ps1
+++
@@ -1,5 +0,0 @@
-cd C:\GitHub-Repos\m365-admin-tool\src\
-
-Get-Content ".\M365AdminTool.ps1" -Raw |
-Invoke-Formatter -Settings "..\PSScriptAnalyzerSettings.psd1" |
-Set-Content ".\M365AdminTool.ps1"
